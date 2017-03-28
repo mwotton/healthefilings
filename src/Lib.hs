79a666c71c6b9eaddb8e79d8b6c19c3cdbc73f30
Mark Wotton <mwotton@gmail.com>
2017-03-28 16:52:20 -0400

    finish qc checks

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( runProgram
     , readInput
     , extractOutput
     , Err
-    , State
-    , Operation
+    , State (..)
+    , Operation (..)
     ) where
 
-import           Data.Text
+import           Control.Monad (foldM)
+import           Data.Monoid   ((<>))
+import           Safe          (atMay)
+import           Text.Read     (readMaybe)
 
 data Operation
-  = Append Text
-  | Delete Int
-  | Print Int
+  = Append String
+  | Delete Word
+  | Print Word
   | Undo
+  deriving Show
 
 data Err
   = BadInput
@@ -22,16 +27,58 @@ data Err
   | PrintOutOfBounds
   deriving (Eq,Show)
 
-data State = State
+data State = State { history :: [String]
+                   , current :: String
+                   , output  :: String
+                   }
+  deriving (Show,Eq)
+
+emptyState = State [] "" ""
 
 runProgram :: [Operation] -> Either Err State
-runProgram = error "runprogram"
+runProgram ops = foldM applyOp emptyState ops
+
+applyOp  st@(State hist curr out) op =
+  case op of
+    Append t -> pure $ st { history = curr : hist
+                          , current = curr <> t }
+    Delete i -> do
+      (fromIntegral i >= length curr) `alternatively` DeletedMoreThanWeHad
+      pure $ st { history = curr : hist
+                , current = (reverse $ drop (fromIntegral i) $ reverse $ curr) }
+    Print i  ->
+      -- 1-based indexing
+      case atMay curr (fromIntegral i - 1) of
+        Nothing -> Left PrintOutOfBounds
+        Just c -> pure $ st { output = c : out }
+    Undo ->
+      case hist of
+        [] -> Left TooManyUndos
+        (x:xs) -> pure $ st { history = xs, current = x }
+
+alternatively :: Bool -> a -> Either a ()
+alternatively True _ = Right ()
+alternatively False a = Left a
 
 readInput :: String -> Either Err [Operation]
-readInput = error "readinput"
+readInput = mapM readCommand . drop 1 . lines
+
+readCommand :: String -> Either Err Operation
+readCommand x =
+  case words x of
+    ["1", x] -> return $ Append x
+    ["2", x] -> Delete <$> getInt x
+    ["3", x] -> Print <$> getInt x
+    ["4"]    -> return Undo
+    _        -> Left BadInput
+
+  where getInt = upgradeMaybe BadInput . readMaybe
+
+upgradeMaybe _ (Just x) = Right x
+upgradeMaybe x _        = Left x
 
 writeOutput :: State -> IO ()
 writeOutput = error "writeoutput"
 
 extractOutput :: State -> [Char]
-extractOutput = undefined
+extractOutput = reverse . output
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,44 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-import           Control.Monad ((<=<))
+{-# LANGUAGE TemplateHaskell     #-}
+import           Control.Monad         ((<=<))
+import           Data.DeriveTH
+import           Data.Monoid
 import           Lib
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck       hiding (output)
 
-main = hspec spec
+derive makeArbitrary ''Operation
+
 
 runFile :: FilePath -> IO (Either Err [Char])
 runFile file = (fmap extractOutput . runProgram <=< readInput) <$> readFile file
 
+
+main = hspec spec
 spec =
   describe "editor" $ do
     it "operates correctly on given test cases" $ do
       runFile "fixtures/sample.txt" `shouldReturn` Right "cya"
+
+    modifyMaxSuccess (const 1000) $
+      prop "allows arbitrary insertion & deletion" $
+      \(before::[Operation],s::String) ->
+        successfulProgramsOnly before (before <> [Append s, Delete (fromIntegral $ length s)]) equivModuloHistory
+
+    modifyMaxSuccess (const 1000) $
+      prop "can undo anything but a print or an undo" $
+      \(ops::[Operation],extraOp) ->
+        (case extraOp of
+            Append _ -> True
+            Delete _ -> True
+            _        -> False) ==>
+        successfulProgramsOnly (ops <> [extraOp,Undo]) ops equivModuloHistory
+
+successfulProgramsOnly p1 p2 equivalency =
+  case (runProgram p1, runProgram p2) of
+    (Right s1, Right s2) -> equivalency s1 s2
+    -- this is a bit grotty, but unsure how to pass.
+    _ -> 1 === 1
+
+equivModuloHistory s1 s2 = (current s1, output s1) === (current s2, output s2)
